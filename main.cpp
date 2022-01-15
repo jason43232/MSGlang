385fab4106db3e905a47c5f1b0f2dd98dab3a05c
Ιάσων Φίλιππος Νταγιάννης <csd3540@csd.uoc.gr>
2022-01-15 14:10:48 +0200

    Co-authored-by: Stelios Michelakakis <stelios-michelakakis@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-#include "MSGlang.hpp"
-
+#include "MSGlang.h"
+#define let auto
+#define object object(_VA_LIST)
 int main()
+
 {
-    std::cout << "Hello, World!" << std::endl;
+    let o1 = object;
+    o1.setName("o1");
+    cout << "Object name: " << o1.getName() << endl;
+    o1.addPair("id1", "value1");
+    cout << o1.getPairs().size();
+
     return 0;
 }
